6d8084093d7b036ea6d0a2886f4d36087e44a83c
Ben <ben.m.weber@gmail.com>
2019-05-23 20:25:44 +0200

    initial commit2

--- app/src/main/java/com/example/spikeball/MainActivity.kt
+++ app/src/main/java/com/example/spikeball/MainActivity.kt
@@ -8,7 +8,7 @@ import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
